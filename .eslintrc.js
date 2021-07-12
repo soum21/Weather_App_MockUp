fcab014181a0a6501fc80cadef66b0dfeda42af5
towfiq <soum.ragan@gmail.com>
2021-07-13 01:28:14 +0800

    setting up eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'node': true
-    },
-    'extends': [
-        'plugin:react/recommended',
-    ],
-    'parserOptions': {
-        'ecmaFeatures': {
-            'jsx': true
-        },
-        'ecmaVersion': 12,
-        'sourceType': 'module'
-    },
-    'plugins': [
-        'react'
-    ],
-    'rules': {
-        'semi': ['error', 'always'],
-        'quotes': ['error', 'single'],
-        'curly': 'error',
-        'eqeqeq': ['error', 'smart'],
-        'no-var': 'error',
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'react-app',
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:prettier/recommended',
+		'prettier',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['react', 'prettier'],
+	rules: {
+		semi: ['error', 'always'],
+		quotes: ['error', 'single'],
+		curly: 'error',
+		eqeqeq: ['error', 'smart'],
+		'no-var': 'error',
 		'no-unused-vars': 'warn',
-        'no-empty': 'off',
-        'no-eval': 'error',
+		'no-empty': 'off',
+		'no-eval': 'error',
 		'no-floating-decimal': 'error',
-        'no-multi-spaces': 'error',
-        'camelcase': [2, { properties: 'always' }],
-        
-    }
+		'no-multi-spaces': 'error',
+		camelcase: [2, { properties: 'always' }],
+	},
 };
--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 function App() {
-    return (
-      <div>
-        HEllo
-      </div>
-    );
-  }
-  
-  export default App;
+	return <div>HEllo</div>;
+}
+
+export default App;
